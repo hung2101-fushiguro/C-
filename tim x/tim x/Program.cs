68685ffb3b9283bc013bf434eb61f43faa4b4071
Satoru <joonheung2101@gmail.com>
2025-10-23 15:51:05 +0700

    them bai tap

---
+++ tim x/tim x/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+internal class FindX
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.WriteLine("Enter a, b, c: ax+b=c");
+        Console.WriteLine("a");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("b");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("c");
+        c = Convert.ToDouble(Console.ReadLine());
+        if (a == 0)
+        {
+            Console.WriteLine("Error");
+        }
+        else
+        {
+            double x = (c - b) / a;
+            Console.WriteLine("x = " + x);
+            Console.Read();
+        }
+    }
+}
