d1522ce5c6b812a91ae7d56cc46e96ecfa0d1bad
Satoru <joonheung2101@gmail.com>
2025-10-23 14:58:47 +0700

    input data

---
+++ Readline/Readline/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;   
+internal class MyClass
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        int age;
+        Console.WriteLine("Enter your name:");
+        name = Console.ReadLine()!;
+        Console.WriteLine("Enter your age:");
+        string userInput = Console.ReadLine()!;
+        age = Convert.ToInt32(userInput);
+        Console.WriteLine("{0} {1} year olds", name, age);
+        Console.ReadLine();
+    }
+}
