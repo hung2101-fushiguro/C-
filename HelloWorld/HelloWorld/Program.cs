fee3a4fe01c886b5ac221ba3c2fd81bc19409b7c
Satoru <joonheung2101@gmail.com>
2025-10-23 14:31:41 +0700

    hello world

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int firstNumber = 5, secondNuber = 10;
+            int result = firstNumber + secondNuber;
+            Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNuber, result);
+            Console.Read();
+           
+        }
+    }
+
+}
