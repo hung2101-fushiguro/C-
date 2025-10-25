e34dd2bdb3e620d88013ed7195d87ac8e65da7d8
Satoru <joonheung2101@gmail.com>
2025-10-25 23:12:00 +0700

    bai 14

---
+++ Bai tap/calculator/calculator/Method/Calculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Method
+{
+    public class Calculation
+    {
+        public double sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double divide(double a, double b)
+        {
+            if (b == 0)
+            {
+              Console.WriteLine("error!");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Bai tap/calculator/calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using calculator.Method;
+using System;
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Enter a:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Calculation cal = new Calculation();
+            Console.WriteLine("Sum: {0}", cal.sum(a, b));
+            Console.ReadLine();
+         
+        }
+    }
+}
