8f6d61f416b7493ee4d5f2308fc3c3a6f87a147d
Satoru <joonheung2101@gmail.com>
2025-10-29 13:52:23 +0700

    bai 15

---
+++ Method/Method/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Method
+{
+    public class Method
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(addition(10, 60));
+        }
+        static int addition(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
